200d3e11314f0e7adf8ee1ffb500d1ae156fa7b9
Коннов Игорь <ikonnoff@mail.ru>
2026-06-02 14:04:01 +0300

    Sprint3 Порефакторил юнит-тесты

--- src/main/java/ru/yandex/practicum/dto/NewPostDto.java
+++ src/main/java/ru/yandex/practicum/dto/NewPostDto.java
@@ -11,23 +11,32 @@ public record NewPostDto(
         // В задании не сказано, но логично, что нужно ограничить длину заголовка, текста, кол-во и длину тегов
         // В реальности нужно уточнить у PM, но для примера пока выставил примерные лимиты соцсетей
 
-        @NotBlank(message = "Заголовок поста обязателен")
-        @Size(max = TITLE_MAX_LENGTH, message = "Заголовок поста не должен превышать " + TITLE_MAX_LENGTH + " символов")
+        @NotBlank(message = MSG_TITLE_REQUIRED)
+        @Size(max = TITLE_MAX_LENGTH, message = MSG_TITLE_MAX_LENGTH)
         String title,
 
-        @NotBlank(message = "Текст поста обязателен")
-        @Size(max = TEXT_MAX_LENGTH, message = "Текст поста не должен превышать " + TEXT_MAX_LENGTH + " символов")
+        @NotBlank(message = MSG_TEXT_REQUIRED)
+        @Size(max = TEXT_MAX_LENGTH, message = MSG_TEXT_MAX_LENGTH)
         String text,
 
-        @NotEmpty(message = "Должен быть указан хотя бы один тег")
-        @Size(max = TAGS_MAX_COUNT, message = "Можно указать не более " + TAGS_MAX_COUNT + " тегов")
-        List<@Size(max = TAG_MAX_LENGTH, message = "Длина тега не должна превышать " + TAG_MAX_LENGTH + " символов") String> tags
+        @NotEmpty(message = MSG_TAGS_REQUIRED)
+        @Size(max = TAGS_MAX_COUNT, message = MSG_TAGS_MAX_COUNT)
+        List<@Size(max = TAG_MAX_LENGTH, message = MSG_TAG_MAX_LENGTH) String> tags
 ) {
 
         public static final int TITLE_MAX_LENGTH = 128;
         public static final int TEXT_MAX_LENGTH = 4096;
         public static final int TAGS_MAX_COUNT = 10;
         public static final int TAG_MAX_LENGTH = 25;
+
+        public static final String MSG_TITLE_MAX_LENGTH = "Заголовок поста не должен превышать " + TITLE_MAX_LENGTH + " символов";
+        public static final String MSG_TEXT_MAX_LENGTH = "Текст поста не должен превышать " + TEXT_MAX_LENGTH + " символов";
+        public static final String MSG_TAG_MAX_LENGTH = "Длина тега не должна превышать " + TAG_MAX_LENGTH + " символов";
+        public static final String MSG_TAGS_MAX_COUNT = "Можно указать не более " + TAGS_MAX_COUNT + " тегов";
+
+        public static final String MSG_TITLE_REQUIRED = "Заголовок поста обязателен";
+        public static final String MSG_TEXT_REQUIRED = "Текст поста обязателен";
+        public static final String MSG_TAGS_REQUIRED = "Должен быть указан хотя бы один тег";
 
 }
 
